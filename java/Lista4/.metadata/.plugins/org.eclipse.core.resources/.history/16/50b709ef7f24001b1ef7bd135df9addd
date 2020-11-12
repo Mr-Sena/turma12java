ed8651e7473274e914a7607e8d0fd6581151be22
Mr-Sena <jsenna985@gmail.com>
2020-11-11 22:12:40 -0300

    Arrays & Matrizes, o vetor dos vetores

---
+++ java/Lista4/.metadata/.plugins/org.eclipse.core.resources/.history/16/50b709ef7f24001b1ef7bd135df9addd
@@ -0,0 +1,19 @@
+package Vetor;
+import java.util.Scanner;
+public class Score {
+	
+	public static void main(String[] args) {
+	
+		double maiorPontuacao = 0;
+		Scanner entrada = new Scanner(System.in);
+		double sequencia[] = new double[10];
+		
+		for (int x = 0; x < 5; x++) {
+			System.out.print((x + 1) + "ª Pontuação: ");
+			sequencia[x] = entrada.nextDouble();
+			if (sequencia[x] > maiorPontuacao) {
+				maiorPontuacao = sequencia[x];
+			}
+		}System.out.println("Maior pontuação registrada: " + maiorPontuacao);
+	}
+}
---
+++ java/Lista4/Exercito/src/Matriz/OperacaoMatriz.java
@@ -0,0 +1,26 @@
+package Matriz;
+import java.util.*;
+public class OperacaoMatriz {
+
+	public static void main(String[] args) {
+		int somaTotal = 0, somaDiagonal = 0;
+
+		Scanner entrada = new Scanner(System.in);
+		
+		int matrix[][] = new int[3][3];
+		
+		System.out.println("Alimentação da Matriz");
+		
+		for(int column = 0; column < 3; column++) {
+			for(int line = 0; line < 3; line++) {
+				System.out.printf("Um valor para a matriz na posição [%d][%d]: ", line, column);
+				matrix[line][column] = entrada.nextInt();
+				somaTotal += matrix[line][column];
+				if (line == column) {
+					somaDiagonal += matrix[line][column];
+				}
+			}
+		}System.out.println("Soma de todos os valores: " + somaTotal);
+		System.out.println("Soma dos valores na diagonal principal: " + somaDiagonal);
+	}
+}
